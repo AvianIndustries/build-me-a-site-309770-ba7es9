a7d35c0d8506bc8e199b48d0cc1323a2f1bb9bb2
AvianIndustries <62577392+AvianIndustries@users.noreply.github.com>
2025-09-21 16:32:18 -0500

    AI generated: components/Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className='bg-blue-500 text-white text-lg p-4'>
+      <h1 className='text-center'>Welcome to Doggo World!</h1>
+    </header>
+  );
+}
